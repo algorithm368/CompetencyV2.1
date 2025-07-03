6e6f05a90cf85d6da2417d7c22265777d48769a1
siriwatchairak <earchchairuk@gmail.com>
2025-07-03 18:44:17 +0700

    UPDATE

--- Server/src/modules/competency/services/searchCareerServices.ts
+++ Server/src/modules/competency/services/searchCareerServices.ts
@@ -1,46 +1,56 @@
-import { PrismaClient } from "@prisma/client_sfia";
+import { prismaSfia, prismaTpqi } from "@Database/prismaClients";
 
-const prisma = new PrismaClient();
+type DBType = "sfia" | "tpqi"
 
 // Get all job name
-async function getJobs() {
-  try {
-    const jobs = await prisma.jobs.findMany({
-      select: {
-        job_name: true,
-      },
-      orderBy: {
-        job_name: "asc",
-      },
-    });
-    return jobs.map((job) => job.job_name);
-  } catch (error) {
-    console.error("Error fetching job names:", error);
-    throw error;
+async function getJobs(dbType: DBType) {
+  if (dbType === "sfia") {
+    try {
+      const jobs = await prismaSfia.jobs.findMany({
+        select: { job_name: true },
+        orderBy: { job_name: "asc" },
+      });
+      return jobs.map((job: any) => job.job_name);
+    } catch (error) {
+      console.error("Error fetching SFIA job names:", error);
+      throw error;
+    }
+  } else {
+    try {
+      const careers = await prismaTpqi.occupational.findMany({
+        select: { name_occupational: true },
+        orderBy: { name_occupational: "asc" },
+      });
+      return careers.map((career: any) => career.name_occupational);
+    } catch (error) {
+      console.error("Error fetching TPQI career names:", error);
+      throw error;
+    }
   }
 }
 
-//
-async function searchJobByName(searchTerm: string) {
-  try {
-    const jobs = await prisma.jobs.findMany({
-      where: {
-        job_name: {
-          contains: searchTerm,
-        },
-      },
-      select: {
-        job_name: true,
-      },
-      orderBy: {
-        job_name: "asc",
-      },
-    });
-    return jobs.map((job) => job.job_name);
-  } catch (error) {
-    console.error("Error searching jobs by name:", error);
-    throw error;
-  }
-}
+getJobs("tpqi").then((job) => console.log(job));
 
-searchJobByName("busi").then((job) => console.log(job));
+// async function searchJobByName(searchTerm: string) {
+//   try {
+//     const jobs = await prisma.jobs.findMany({
+//       where: {
+//         job_name: {
+//           contains: searchTerm,
+//         },
+//       },
+//       select: {
+//         job_name: true,
+//       },
+//       orderBy: {
+//         job_name: "asc",
+//       },
+//     });
+//     return jobs.map((job) => job.job_name);
+//   } catch (error) {
+//     console.error("Error searching jobs by name:", error);
+//     throw error;
+//   }
+// }
+
+// searchJobByName("busi").then((job) => console.log(job));

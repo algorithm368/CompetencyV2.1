61a9262be6082d0029f9643811a04b705f0efdd8
siriwatchairak <earchchairuk@gmail.com>
2025-07-09 19:36:18 +0700

    UPDATE

--- Server/src/modules/sfia/routes/getJobLevelsRoutes.ts
+++ Server/src/modules/sfia/routes/getJobLevelsRoutes.ts
@@ -1,6 +1,5 @@
 import { Router, RequestHandler } from "express";
 import { getJobLevelsController } from "../controllers/getJobLevelsController";
-import { getJobDetailController } from "../controllers/getJobDetailController";
 
 const router = Router();
 
@@ -17,6 +16,5 @@ const router = Router();
  *   ]
  */
 router.get("/:jobCode", getJobLevelsController as RequestHandler);
-router.get("/:jobCode", getJobDetailController as RequestHandler);
 
 export default router;
